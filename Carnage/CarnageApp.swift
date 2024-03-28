6aa298e736e812705893e0da2f5bb6dbccb45c03
Raveen Perera <raveenperera@icloud.com>
2024-03-28 20:20:44 +0530

    Initial Commit

---
+++ Carnage/CarnageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarnageApp.swift
+//  Carnage
+//
+//  Created by Raveen Perera on 2024-03-28.
+//
+
+import SwiftUI
+
+@main
+struct CarnageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
